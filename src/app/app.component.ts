d677e856b9831ecdef67d862c369f36855414c16
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-07-01 19:17:09 -0400

    Inital Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   title = 'f-store';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/www/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomePageComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
